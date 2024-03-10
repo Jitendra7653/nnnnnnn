8785344ff673d9481e7e2809c1493e6b2c9cfa48
Jitendra7653 <96977015+Jitendra7653@users.noreply.github.com>
2024-03-10 20:33:23 +0530

    Create about.py

---
+++ plugins/about.py
@@ -0,0 +1,14 @@
+import os 
+from pyrogram import Client, filters
+token = BOT_TOKEN
+botid = token.split(':')[0]
+from helper.database import botdata, find_one, total_user
+
+from helper.progress import humanbytes
+
+@Client.on_message(filters.private & filters.command(["about"]))
+async def start(client,message):
+	botdata(int(botid))
+	data = find_one(int(botid))
+	total_rename = data["total_rename"]
+	total_size = data["total_size"]
